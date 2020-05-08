876614cc1eb9fc5949959e521e2cdeb3b48db93f
Darciechapman <darciechapman96@gmail.com>
2020-05-08 20:51:10 +0800

    fixing indents

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require("inquirer");
-const mysql=require("mysql");
+const mysql = require("mysql");
 ​
-const con=mysql.createConnection({
+const con = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'root',
@@ -22,20 +22,20 @@ con.connect(function(err){
 ​
 function action() {
     inquirer.prompt([
-      {
-        type: "list",
-        name: "action",
-        message: "What would you like to do?",
-        choices: [
-          "View all employees",
-          "View all employees by department",
-          "View all employees by manager",
-          "Add Employee",
-          "Remove employee",
-          "Update employee role",
-          //"Update manager role"
-        ]
-      }
+        {
+            type: "list",
+            name: "action",
+            message: "What would you like to do?",
+            choices: [
+                "View all employees",
+                "View all employees by department",
+                "View all employees by manager",
+                "Add Employee",
+                "Remove employee",
+                "Update employee role",
+                //"Update manager role"
+            ]
+        }
     ])}
     // if view all employees, display all employee data 
     // if view all employees by department
