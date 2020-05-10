8ceed26dbc2559805905f2acabbb8ac8af07aa6a
Darciechapman <darciechapman96@gmail.com>
2020-05-10 21:41:25 +0800

    displaying employee table in console

--- server.js
+++ server.js
@@ -17,45 +17,49 @@ con.connect(function(err){
     //this won't run if you have an error
     console.log("success!");
     let queryTxt = "select from employees"
-    con.query(queryTxt, function(error, result) {
-        //console logs data (dont actually have data yet)
-        console.log(result)
-    });
     //action();
+})
+
+con.query(
+    "SELECT `first_name`, `last_name`, `title` as 'role' FROM employeesdb.employee inner join employeesdb.roles on(employee.role_id = roles.id);",
+    function(error, result, fields) {
+    //console logs data (dont actually have data yet)
+    console.table(result)
+    action()
 });
 
-// function action() {
-//     inquirer.prompt([
-//         {
-//             type: "list",
-//             name: "action",
-//             message: "What would you like to do?",
-//             choices: [
-//                 "View all employees",
-//                 "View all employees by department",
-//                 "View all employees by manager",
-//                 "Add Employee",
-//                 "Remove employee",
-//                 "Update employee role",
-                // "Update manager role"
-//             ]
-//         }
-//     ]).then(qry_txt => {
-//         console.log(qry_txt)
-//         if (qry_txt === "View all employees") {
-//             viewEmployees()
-//         } if (qry_txt === "View all employees by department") {
-//             viewEmployeesDepartment()
-//         } if (qry_txt === "View all employees by manager") {
-//             viewMangers()
-//         } if (qry_txt === "Add Employee") {
-//             addEmployees()
-//         } if (qry_txt === "Remove employee") {
-//             removeEmployees()
-//         } else {
-//             UpdateEmployees()
-//         }
-//     });
+function action() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "action",
+            message: "What would you like to do?",
+            choices: [
+                "View all employees",
+                "View all departments",
+                "View all employees by manager",
+                "Add Employee",
+                "Remove employee",
+                "Update employee role",
+                //"Update manager role"
+            ]
+        }
+    ]).then(({ action }) => {
+        console.log(action)
+        if (action === "View all employees") {
+            viewEmployees()
+        } else if (action === "View all departments") {
+            viewDepartment()
+        } else if (action === "View all employees by manager") {
+            viewMangers()
+        } else if (action === "Add Employee") {
+            addEmployees()
+        } else if (action === "Remove employee") {
+            removeEmployees()
+        } else {
+            UpdateEmployees()
+        }
+    });
     // if view all employees, display all employee data 
     // if view all employees by department
     // -- prompt department selection
@@ -63,14 +67,69 @@ con.connect(function(err){
     // -- display manager list
     // if add
 
-    //.then(qry_text =>{
-    //    console.log(qry_text);
-    //    con.query(qry_text,function(err,result){
-    //        console.log(result);
-    //    })
-    //    con.end();
-// }
+}
 
-// function viewEmployees() {
-//     console.log("clicked")
-// }
+function viewEmployees() {
+    con.query(
+        "SELECT `first_name`, `last_name`, `title` as 'role' FROM employeesdb.employee inner join employeesdb.roles on(employee.role_id = roles.id);",
+        function(error, result, fields) {
+            console.table(result)
+        });
+}
+
+function viewDepartment() {
+    con.query(
+        "SELECT `title` as 'department' FROM employeesdb.department;",
+        function(error, result, fields) {
+            console.table(result)
+        });
+}
+
+function viewMangers() {
+    con.query(
+        "SELECT `first_name`, `last_name`, `title` as 'manager' WHERE FROM employeesdb.employee inner join employeesdb.roles on(employee.role_id = roles.id);",
+        function(error, result, fields) {
+            console.table(result)
+        }
+    );
+}
+
+function addEmployees() {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "firstName",
+            message: "what is your employee's first name?"
+        },
+        {
+            type: "input",
+            name: "lastName",
+            message: "what is your employee's last name?"
+        },
+        {
+            type: "input",
+            name: "role",
+            message: "what is the role Id for thee employee?"
+        },
+        {
+            type: "input",
+            name: "manager",
+            message: "what is your manager's Id?"
+        }
+    ]).then((firstName,lastName,role,manager) => {
+        con.query(
+            `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (${firstName},${lastName},${role},${manager});`,
+            function(error, result, fields) {
+                console.log(result)
+            }
+        )
+    })
+}
+
+function removeEmployees() {
+    
+}
+
+function UpdateEmployees() {
+    
+}
